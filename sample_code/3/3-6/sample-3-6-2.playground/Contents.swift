5322c4eff23cb2ae54013711d6d8e5e7a929d632
Masaki Kitagawa <kitagawa.stilo@gmail.com>
2018-06-01 19:34:38 +0900

    Change method to property, use struct

--- sample_code/3/3-6/sample-3-6-2.playground/Contents.swift
+++ sample_code/3/3-6/sample-3-6-2.playground/Contents.swift
@@ -6,32 +6,18 @@
 
 import Foundation
 
-// BookModelにメソッドを追加
-class BookModel: NSObject {
-    var price: Int = 10000          // 価格
+struct Book {
+    let price: Int
     
-    // 価格を3桁区切りの文字列で返すメソッド
-    func decimalPrice() -> String {
-        // NSNumberへ変換（NumberFormatterを使用するため）
-        let num: NSNumber = NSNumber(value: price)
-
-        // NumberFormatterのインスタンスを定義
-        let numberFormatter = NumberFormatter()
-
-        // 十進法の数値として取り扱う
-        numberFormatter.numberStyle = NumberFormatter.Style.decimal
-
-        // 区切りに使用する文字を定義（","で区切る）
-        numberFormatter.groupingSeparator = ","
-
-        // 何桁区切りにするか（3桁区切り）
-        numberFormatter.groupingSize = 3
-
-        // 変換して返す（10,000を返す）
-        return numberFormatter.string(from: num)!
+    var decimalPriceString: String {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.groupingSeparator = ","
+        nf.groupingSize = 3
+        return nf.string(from: NSNumber(value: price))!
     }
 }
 
-let model:BookModel = BookModel()
-print(model.decimalPrice())
+let book = Book(price: 10000)
+print(book.decimalPriceString)
 // 10,000
